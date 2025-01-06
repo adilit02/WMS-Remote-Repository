94322c510dd7d4e233ab314159dc6075439035de
sanjay-2224 <sanjayghisadi@gmail.com>
2025-01-06 15:15:11 +0530

    Shipment Update And Delete URL Added

--- src/main/java/ait/com/controller/ShipmentController.java
+++ src/main/java/ait/com/controller/ShipmentController.java
@@ -1,6 +1,7 @@
 package ait.com.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import ait.com.entity.Order;
 import ait.com.entity.Shipment;
@@ -18,7 +20,7 @@ public class ShipmentController {
 
 	@Autowired
 	private ShipmentService shipService;
-	
+
 	@GetMapping("/shipmentorder")
 	public String OrderMaster(Model model) {
 		Shipment shipment = new Shipment();
@@ -27,29 +29,46 @@ public class ShipmentController {
 
 		return "ShipmentPage";
 	}
-	
+
 	@PostMapping("/createshipment")
 	public String saveShipment(@ModelAttribute("ship") Shipment shipment, Model model) {
 
 		Shipment save = shipService.createdShipment(shipment);
 
-		String msg = (save!=null) ? "Shipment Sucsess....." : "Shipment Not Sucsess....";
+		String msg = (save != null) ? "Shipment Sucsess....." : "Shipment Not Sucsess....";
 
 		model.addAttribute("msg", msg);
 
 		return "ShipmentPage";
 	}
-	
+
 	@GetMapping("/shipmentview")
-	public String getAllShipment( @ModelAttribute("ship") Shipment shipment,Model model)
-	{
-		List<Shipment> list=shipService.getAllShipment();
-		
+	public String getAllShipment(@ModelAttribute("ship") Shipment shipment, Model model) {
+		List<Shipment> list = shipService.getAllShipment();
+
 		model.addAttribute("shiptlist", list);
-		
+
 		return "ShipmentView";
 	}
 
-	//____________________Update And Delete Url Painding_________________________
-	
+	@GetMapping("/shipmentupdate")
+	public String UserUpadte(@RequestParam("shipmentId") Integer Id, Model model) {
+
+		Optional<Shipment> ship_upd = shipService.getShipmentById(Id);
+
+		System.out.println("======>" + Id);
+		model.addAttribute("ship", ship_upd);
+		return "ShipmentUpdate";
+	}
+
+	@GetMapping("/shipmentdelete")
+	public String deleteUser(@RequestParam("shipmentId") Integer Id, Model model) {
+
+		shipService.deleteShipment(Id);
+
+		model.addAttribute("shiptlist", shipService.getAllShipment());
+		// return "redirect:view";
+		return "ShipmentView";
+	}
+
 }
--- src/main/java/ait/com/serviceImpl/ShipmentServiceImp.java
+++ src/main/java/ait/com/serviceImpl/ShipmentServiceImp.java
@@ -3,6 +3,7 @@
 package ait.com.serviceImpl;
 
 import java.util.List;
+
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
